bd6db65d125fc414291523d83b7ceea3d1eaf7a8
kaycout <kaycoutinho387@gmail.com>
2026-05-14 20:27:42 -0300

    Corrigi a estrutura  e organizei os arquivos

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-// arquivo app/index.tsx
-// aqui a tela inicial do app redirecionando para o login
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  return <Redirect href="/login" />;
-}
